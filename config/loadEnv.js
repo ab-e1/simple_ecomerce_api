43514146815b047f98d04690b9728b5a2fd443f6
ab-e1 <abelghy17@gmail.com>
2026-08-30 19:40:56 -0400

    made small changes

--- config/loadEnv.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  port: process.env.PORT || 3000,
-  jwtSecret:
-    process.env.JWT_SECRET || "default_jw_secret_change_it_imediately_or_soon",
-  mongoDbUri: process.env.MONGO_URI,
-  mongoDbPassword: process.env.MONGO_PASSWORD,
-};
---
+++ src/services/authService.js
@@ -0,0 +1,4 @@
+import { argon2 } from "argon2";
+import { signToken } from "../utils/jwt";
+
+export const register = async (data) => {};
