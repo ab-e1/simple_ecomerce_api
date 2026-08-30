1dbba5770ff6c70ee87197ca7e586ff6a930b165
ab-e1 <abelghy17@gmail.com>
2026-08-30 06:52:02 -0400

    changed the import export from commonJs to ESModules

--- src/config/loadEnv.js
+++ src/config/loadEnv.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
+import dotenv from "dotenv";
 
-module.exports = {
-  port: process.env.PORT || 3000,
-  jwtSecret:
-    process.env.JWT_SECRET || "default_jw_secret_change_it_imediately_or_soon",
-  mongoDbUri: process.env.MONGO_URI,
-  mongoDbPassword: process.env.MONGO_PASSWORD,
-};
+dotenv.config();
+export const port = process.env.PORT || 3000;
+export const jwtSecret =
+  process.env.JWT_SECRET || "default_jw_secret_change_it_imediately_or_soon";
+export const mongoDbUri = process.env.MONGO_URI;
+export const mongoDbPassword = process.env.MONGO_PASSWORD;
