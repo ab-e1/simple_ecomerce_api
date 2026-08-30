0b0fada9f4dfd87192f5040f1c5a6b4661ddce13
ab-e1 <abelghy17@gmail.com>
2026-08-30 06:46:24 -0400

    initialted the app.use, and app.listen port inthe srver and app.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
+import { port } from "./config/loadEnv.js";
+import { app } from "./app.js";
+
 console.log("Hello via Bun!");
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port} `);
+});
