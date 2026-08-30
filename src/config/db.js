0f1b608f2afd2508aa1b733cc9be6a0f0b8e948c
ab-e1 <abelghy17@gmail.com>
2026-08-30 06:50:54 -0400

    added the mongoos connection with the mongodb databse

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { mongoDbUri } from "./loadEnv";
+
+export const connectDb = async () => {
+  if (!mongoDbUri) {
+    return {
+      ok: false,
+      error:
+        "MONGO_URI missing in the .env .copy from env.example, and set the value",
+    };
+  }
+  await mongoose.connect(mongoDbUri);
+  return {
+    ok: true,
+    data: "monogdb connected",
+  };
+};
