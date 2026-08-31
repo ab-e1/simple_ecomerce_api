7b9f7fcf8db358a71a8b7c1405e6b589203308f2
ab-e1 <abelghy17@gmail.com>
2026-08-31 19:39:29 -0400

    add: added the logger middlware ot be used , and also product toute so the app.js use it

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import authRoute from "./routes/authRoute.js";
+import logger from "./middleware/logger.js";
+import productRoute from "./routes/productRoute.js"
 
 export const app = express();
 app.use(express.json());
+app.use(logger);
 
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRoute);
