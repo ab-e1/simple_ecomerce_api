d5341b97869a7ffdf6fb629faf97be2870c4d064
ab-e1 <abelghy17@gmail.com>
2026-08-31 15:18:55 -0400

    fix: fixed a typo in the schema field

--- src/model/product.js
+++ src/model/product.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     price: { type: Number, required: true },
     description: { type: String, required: false },
-    catagory: { type: String, required: false },
+    category: { type: String, required: false },
     inStock: { type: Boolean, default: true },
   },
   { timestamps: true },
