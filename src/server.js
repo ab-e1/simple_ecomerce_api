28518d21de991ff95d1c6ffed7c9a2a9cfe8437d
ab-e1 <abelghy17@gmail.com>
2026-08-31 15:20:12 -0400

    add: added the connectDb to run before the app.listen when the app starts

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import { port } from "./config/loadEnv.js";
 import { app } from "./app.js";
+import { connectDb } from "./config/db.js";
 
+connectDb();
 console.log("Hello via Bun!");
 
 app.listen(port, () => {
