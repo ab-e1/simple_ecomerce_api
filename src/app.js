bf8097a07ce193427bd1d20d581a0e3310a266e8
ab-e1 <abelghy17@gmail.com>
2026-09-02 01:41:25 +0300

    add: added the swagger to our app.js so express uses it

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import authRoute from "./routes/authRoute.js";
 import logger from "./middleware/logger.js";
 import productRoute from "./routes/productRoute.js";
 import cartRoute from "./routes/cartRoute.js";
 import orderRoute from "./routes/orderRoute.js";
+import { swaggerSpec } from "./config/swagger.js";
 import { errorHandeler } from "./middleware/errorHandler.js";
 
 export const app = express();
@@ -14,5 +16,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
 app.use("/api/order", orderRoute);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errorHandeler);
