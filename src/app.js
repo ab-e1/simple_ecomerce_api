05f136114357e9485937f51bf5901fc4e81f5d85
ab-e1 <abelghy17@gmail.com>
2026-09-01 22:17:49 +0300

    add: added the endpoint for the app to use it

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoute from "./routes/authRoute.js";
 import logger from "./middleware/logger.js";
 import productRoute from "./routes/productRoute.js"
+import cartRoute from "./routes/cartRoute.js"
 
 export const app = express();
 app.use(express.json());
@@ -9,3 +10,4 @@ app.use(logger);
 
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
