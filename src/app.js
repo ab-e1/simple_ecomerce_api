4a1d9a6e4ed936b404b5f30dba3eb2ef351b88c9
ab-e1 <abelghy17@gmail.com>
2026-09-02 00:54:45 +0300

    add: added the order path in the app.js so the app could use it

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import authRoute from "./routes/authRoute.js";
 import logger from "./middleware/logger.js";
-import productRoute from "./routes/productRoute.js"
-import cartRoute from "./routes/cartRoute.js"
+import productRoute from "./routes/productRoute.js";
+import cartRoute from "./routes/cartRoute.js";
+import orderRoute from "./routes/orderRoute.js";
 
 export const app = express();
 app.use(express.json());
@@ -11,3 +12,4 @@ app.use(logger);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
