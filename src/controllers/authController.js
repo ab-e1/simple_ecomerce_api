a006e8d572b774226d4566737c8738c4e3b3fb5f
ab-e1 <abelghy17@gmail.com>
2026-09-01 22:30:22 +0300

    fix: added try and catch in all of the functions for error catching

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,26 @@
 import * as authService from "../services/authService.js";
 import { success, failure } from "../utils/response.js";
 
-export const register = async (req, res) => {
-  const result = await authService.register(req.body);
-  if (!result.ok) {
-    return failure(res, result.error, 401);
+export const register = async (req, res, next) => {
+  try {
+    const result = await authService.register(req.body);
+    if (!result.ok) {
+      return failure(res, result.error, 401);
+    }
+    return success(res, result.data, 201, result.token);
+  } catch (err) {
+    next(err)
   }
-  return success(res, result.data, 201, result.token);
-};
+}
 
-export const login = async (req, res) => {
-  const result = await authService.login(req.body);
-  if (!result.ok) {
-    return failure(res, result.error, 401);
+export const login = async (req, res,next) => {
+  try {
+    const result = await authService.login(req.body);
+    if (!result.ok) {
+      return failure(res, result.error, 401);
+    }
+    return success(res, result.data, 200, result.token);
+  } catch(err) {
+    next(err);
   }
-  return success(res, result.data, 200, result.token);
-};
+  };
